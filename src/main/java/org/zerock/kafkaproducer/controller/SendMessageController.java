e6254216d8d7498e4802089aa02790276a466da9
zerock <zk2020308a@gmail.com>
2024-12-11 09:13:17 +0900

    Producer Ex

--- src/main/java/org/zerock/kafkaproducer/controller/SendMessageController.java
+++ src/main/java/org/zerock/kafkaproducer/controller/SendMessageController.java
@@ -1,4 +1,39 @@
 package org.zerock.kafkaproducer.controller;
 
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.kafkaproducer.service.KafkaProducerService;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/producer")
 public class SendMessageController {
+
+    private final KafkaProducerService kafkaProducerService;
+
+    @GetMapping("send")
+    public String[] sendMessage( @RequestParam("msg") String msg) {
+
+
+        log.info("------------------------------");
+
+        log.info(msg);
+
+        log.info("------------------------------");
+
+        kafkaProducerService.sendMessage("sample_topic", msg);
+
+        return new String[] {msg, "sent"};
+
+    }
+
+
 }
--- src/main/java/org/zerock/kafkaproducer/service/KafkaProducerService.java
+++ src/main/java/org/zerock/kafkaproducer/service/KafkaProducerService.java
@@ -1,4 +1,19 @@
 package org.zerock.kafkaproducer.service;
 
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
 }
